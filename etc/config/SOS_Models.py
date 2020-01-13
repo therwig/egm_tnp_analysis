7f578dc4e05262506992e1b0e8c06948000d255b
Christian Herwig <therwig@fnal.gov>
2020-01-12 19:28:05 -0600

    fix use of super()

--- etc/config/SOS_Models.py
+++ etc/config/SOS_Models.py
@@ -33,12 +33,14 @@ class Model(object):
         self.func=[]
         self.params={}
         self.guess={}
+        self.fit_range=() #to reduce failed fit range
     def GetParams(self):
-        print('yoo',self.params)
         return ["{}{}".format(x,list(self.params[x])) for x in self.params]
-        # print ["{}({})".format(x,",".join(str(self.params[x]))) for x in self.params]
-        # return ["{}({})".format(x,",".join(str(self.params[x]))) for x in self.params]
     def GetFunc(self): return self.func
+    def SetFFitRange(self,a,b): 
+        self.fit_range=(a,b)
+    def GetFFitRange(self): 
+        return self.fit_range
     def AddModel(self,m):
         add_func = m.func
         add_params = m.params
@@ -63,117 +65,117 @@ class Model(object):
 #############################################################
 class Gaussian(Model):
     def __init__(self):
-        super(Model,self).__init__()
+        super(Gaussian,self).__init__()
         self.func=[
-            "Gaussian::sigPass(x,meanP,sigmaP)",
-            "Gaussian::sigFail(x,meanF,sigmaF)",
+            "Gaussian::sigPass(x,SPmean,SPsigma)",
+            "Gaussian::sigFail(x,SFmean,SFsigma)",
         ]
         self.params={
-            "meanP":(90,80,100),"sigmaP":(0.9,0.5,5.0),
-            "meanF":(90,80,100),"sigmaF":(0.9,0.5,5.0),
+            "SPmean":(90,80,100),"SPsigma":(0.9,0.5,5.0),
+            "SFmean":(90,80,100),"SFsigma":(0.9,0.5,5.0),
         }
 class DoubleGaussian(Model):
     def __init__(self):
-        super(Model,self).__init__()
+        super(DoubleGaussian,self).__init__()
         self.func=[
-            "Gaussian::sigPass1(x,meanP1,sigmaP1)",
-            "Gaussian::sigFail1(x,meanF1,sigmaF1)",
-            "Gaussian::sigPass2(x,meanP2,sigmaP2)",
-            "Gaussian::sigFail2(x,meanF2,sigmaF2)",
+            "Gaussian::sigPass1(x,SPmean1,SPsigma1)",
+            "Gaussian::sigFail1(x,SFmean1,SFsigma1)",
+            "Gaussian::sigPass2(x,SPmean2,SPsigma2)",
+            "Gaussian::sigFail2(x,SFmean2,SFsigma2)",
             "SUM::sigPass(vFracPass[0.8,0.3,1]*sigPass1, sigPass2)",
             "SUM::sigFail(vFracFail[0.8,0.3,1]*sigFail1, sigFail2)"
         ]
         self.params={
-            "meanP1":(90,80,100),"sigmaP1":(0.9,0.5,5.0),
-            "meanF1":(90,80,100),"sigmaF1":(0.9,0.5,5.0),
-            "meanP2":(90,80,100),"sigmaP2":(0.9,0.5,5.0),
-            "meanF2":(90,80,100),"sigmaF2":(0.9,0.5,5.0),
+            "SPmean1":(90,80,100),"SPsigma1":(0.9,0.5,5.0),
+            "SFmean1":(90,80,100),"SFsigma1":(0.9,0.5,5.0),
+            "SPmean2":(90,80,100),"SPsigma2":(0.9,0.5,5.0),
+            "SFmean2":(90,80,100),"SFsigma2":(0.9,0.5,5.0),
         }
 
 class Voigt(Model):
     def __init__(self):
-        super(Model,self).__init__()
+        super(Voigt,self).__init__()
         self.func=[
-            "Voigtian::sigPass(x, meanP, widthP, sigmaP)",
-            "Voigtian::sigFail(x, meanF, widthF, sigmaF)",
+            "Voigtian::sigPass(x, SPmean, SPwidth, SPsigma)",
+            "Voigtian::sigFail(x, SFmean, SPwidth, SFsigma)",
         ]
         self.params={
-            "meanP":(90,80,100), "widthP":(2.5,2.0,3.0), "sigmaP":(1.0,0.5,2.5),
-            "meanF":(90,80,100), "widthF":(2.5,2.0,3.0), "sigmaF":(1.0,0.5,2.5),
+            "SPmean":(90,80,100), "SPsigma":(1.0,0.5,2.5), "SPwidth":(2.5,2.0,3.0),
+            "SFmean":(90,80,100), "SFsigma":(1.0,0.5,2.5),
         }
 class DoubleVoigt(Model):
     def __init__(self):
-        super(Model,self).__init__()
+        super(DoubleVoigt,self).__init__()
         self.func=[
-            "Voigtian::sigPass1(x, meanP1, width, sigmaP1)",
-            "Voigtian::sigFail1(x, meanF1, width, sigmaF1)",
-            "Voigtian::sigPass2(x, meanP2, width, prod::sigmaP2(sigmaP1, sigmaPRatio))",
-            "Voigtian::sigFail2(x, meanF2, width, prod::sigmaF2(sigmaF1, sigmaFRatio))",
+            "Voigtian::sigPass1(x, SPmean1, SPwidth, SPsigma1)",
+            "Voigtian::sigFail1(x, SFmean1, SPwidth, SFsigma1)",
+            "Voigtian::sigPass2(x, SPmean2, SPwidth, prod::Psigma2(SPsigma1, SPsigmaRatio))",
+            "Voigtian::sigFail2(x, SFmean2, SPwidth, prod::Fsigma2(SFsigma1, SFsigmaRatio))",
             "SUM::sigPass(vFracPass[0.8,0,1]*sigPass1, sigPass2)",
             "SUM::sigFail(vFracFail[0.8,0,1]*sigFail1, sigFail2)"
         ]
         self.params={
-            "meanP1":(90,80,100), "sigmaP1":(1.0,0.5,2.5), "width":(2.5,2.0,3.0),
-            "meanF1":(90,80,100), "sigmaF1":(1.0,0.5,2.5),
-            "meanP2":(90,80,100), "sigmaPRatio":(4,1.5,10),
-            "meanF2":(90,80,100), "sigmaFRatio":(4,1.5,10),
+            "SPmean1":(90,80,100), "SPsigma1":(1.0,0.5,2.5), "SPwidth":(2.5,2.0,3.0),
+            "SFmean1":(90,80,100), "SFsigma1":(1.0,0.5,2.5),
+            "SPmean2":(90,80,100), "SPsigmaRatio":(4,1.5,10),
+            "SFmean2":(90,80,100), "SFsigmaRatio":(4,1.5,10),
         }
-class DoubleVoigt2(Model):
-    def __init__(self):
-        super(Model,self).__init__()
-        self.func=[
-            "Voigtian::sigPass1(x, meanP1, widthP1, sigmaP1)",
-            "Voigtian::sigFail1(x, meanF1, widthF1, sigmaF1)",
-            "Voigtian::sigPass2(x, meanP2, widthP2, prod::sigmaP2(sigmaP1, sigmaPRatio))",
-            "Voigtian::sigFail2(x, meanF2, widthF2, prod::sigmaF2(sigmaF1, sigmaFRatio))",
-            "SUM::sigPass(vFracPass[0.8,0,1]*sigPass1, sigPass2)",
-            "SUM::sigFail(vFracFail[0.8,0,1]*sigFail1, sigFail2)"
-        ]
-        self.params={
-            "meanP1":(90,80,100), "widthP1":(2.5,2.0,3.0), "sigmaP1":(1.0,0.5,2.5),
-            "meanF1":(90,80,100), "widthF1":(2.5,2.0,3.0), "sigmaF1":(1.0,0.5,2.5),
-            "meanP2":(90,80,100), "widthP2":(2.5,2.0,3.0), "sigmaPRatio":(4,1.5,10),
-            "meanF2":(90,80,100), "widthF2":(2.5,2.0,3.0), "sigmaFRatio":(4,1.5,10),
-        }
+# class DoubleVoigt2(Model):
+#     def __init__(self):
+#         super(Model,self).__init__()
+#         self.func=[
+#             "Voigtian::sigPass1(x, meanP1, widthP1, sigmaP1)",
+#             "Voigtian::sigFail1(x, meanF1, widthF1, sigmaF1)",
+#             "Voigtian::sigPass2(x, meanP2, widthP2, prod::sigmaP2(sigmaP1, sigmaPRatio))",
+#             "Voigtian::sigFail2(x, meanF2, widthF2, prod::sigmaF2(sigmaF1, sigmaFRatio))",
+#             "SUM::sigPass(vFracPass[0.8,0,1]*sigPass1, sigPass2)",
+#             "SUM::sigFail(vFracFail[0.8,0,1]*sigFail1, sigFail2)"
+#         ]
+#         self.params={
+#             "meanP1":(90,80,100), "widthP1":(2.5,2.0,3.0), "sigmaP1":(1.0,0.5,2.5),
+#             "meanF1":(90,80,100), "widthF1":(2.5,2.0,3.0), "sigmaF1":(1.0,0.5,2.5),
+#             "meanP2":(90,80,100), "widthP2":(2.5,2.0,3.0), "sigmaPRatio":(4,1.5,10),
+#             "meanF2":(90,80,100), "widthF2":(2.5,2.0,3.0), "sigmaFRatio":(4,1.5,10),
+#         }
 
 #############################################################
 ########## Background models
 #############################################################
 class CMSShape(Model):
     def __init__(self):
-        super(Model,self).__init__()
+        super(CMSShape,self).__init__()
         self.func=[
-            "RooCMSShape::bkgPass(x, acmsP, betaP, gammaP, peakP)",
-            "RooCMSShape::bkgFail(x, acmsF, betaF, gammaF, peakF)",
+            "RooCMSShape::bkgPass(x, BPacms, BPbeta, BPgamma, BPpeak)",
+            "RooCMSShape::bkgFail(x, BFacms, BFbeta, BFgamma, BFpeak)",
         ]
         self.params={
-            "acmsP":(60.,50.,80.),"betaP":(0.05,0.01,0.08),"gammaP":(0.1, -2, 2),"peakP":(90.0,),
-            "acmsF":(60.,50.,80.),"betaF":(0.05,0.01,0.08),"gammaF":(0.1, -2, 2),"peakF":(90.0,),
+            "BPacms":(60.,50.,80.),"BPbeta":(0.05,0.01,0.08),"BPgamma":(0.1, -2, 2),"BPpeak":(90.0,),
+            "BFacms":(60.,50.,80.),"BFbeta":(0.05,0.01,0.08),"BFgamma":(0.1, -2, 2),"BFpeak":(90.0,),
         }
 
 class Exponential(Model):
     def __init__(self):
-        super(Model,self).__init__()
+        super(Exponential,self).__init__()
         self.func=[
-            "RooExponential::bkgPass(x, a0P)",
-            "RooExponential::bkgFail(x, a0F)",
+            "RooExponential::bkgPass(x, BPa0)",
+            "RooExponential::bkgFail(x, BFa0)",
         ]
         self.params={
-            "a0P":(0,-5,5),
-            "a0F":(0,-5,5),
+            "BPa0":(0,-5,5),
+            "BFa0":(0,-5,5),
         }
 
 class ExponentialN(Model):
     '''Exp of an n-dim polynomial '''
     def __init__(self,n):
-        super(Model,self).__init__()
+        super(ExponentialN,self).__init__()
         self.n=n
         self.func=[
-            "RooExponentialN::bkgPass(x,{"+",".join(["a{}P".format(i) for i in range(n)])+"},1)",
-            "RooExponentialN::bkgFail(x,{"+",".join(["a{}F".format(i) for i in range(n)])+"},1)"
+            "RooExponentialN::bkgPass(x,{"+",".join(["BPa{}".format(i) for i in range(n)])+"},1)",
+            "RooExponentialN::bkgFail(x,{"+",".join(["BFa{}".format(i) for i in range(n)])+"},1)"
         ]
         self.params={}
         for i in range(n):
-            self.params["a{}P".format(i)] = (0,-5,5)
-            self.params["a{}F".format(i)] = (0,-5,5)
+            self.params["BPa{}".format(i)] = (0,-5,5)
+            self.params["BFa{}".format(i)] = (0,-5,5)
         print(self.func)
